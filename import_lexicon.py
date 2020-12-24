82f39ea1b6460ca02e671d65d8e20cf66bf73cac
RodgervanderHeijden <rodgervdheijden@hotmail.com>
2020-12-24 14:19:21 +0100

    Fixes #1. Error originates from running app.py as python via PyCharm, rather than the recommended method of the CLI command flask run. Fixed the file path to be relative, but for convenience I might revert it during dev (hence commented code).

--- import_lexicon.py
+++ import_lexicon.py
@@ -2,6 +2,7 @@ import pandas as pd
 
 def import_lexicon():
     # import the vocabulaire
-    file = r'C:\Users\Rodger\Documents\Data Science\[Online courses]\Flask learnings\Julian Nash\app\Lexicon.csv'
+#    file = r'C:\Users\Rodger\Documents\Data Science\[Online courses]\Flask learnings\Julian Nash\app\Lexicon.csv'
+    file = "./Lexicon.csv"
     df = pd.read_csv(file)
     return df
